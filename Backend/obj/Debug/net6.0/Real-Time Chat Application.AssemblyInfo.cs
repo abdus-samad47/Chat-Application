b2dae1e79910c364e2350d4eddcf252dc5a72e02
abdus-samad47 <abdussamadt47@gmail.com>
2024-10-18 09:48:27 +0500

    Task Completed

--- Backend/obj/Debug/net6.0/Real-Time Chat Application.AssemblyInfo.cs
+++ Backend/obj/Debug/net6.0/Real-Time Chat Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Real-Time Chat Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+698767582ad6a1b070544a1907c5d23e930e9dc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+360f6a94469edad4328f121b4c0706dea05e26cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Real-Time Chat Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Real-Time Chat Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
